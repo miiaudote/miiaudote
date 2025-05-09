b51efa77358b9d35ee065504339e4df269b0be94
Emanuel Bevilaqua <beviemanuel@gmail.com>
2025-05-09 19:46:57 -0300

    Major changes

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import *
 from flask_login import *
+
 from flask_wtf import FlaskForm
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 
 from wtforms import *
 from wtforms.validators import *
+
+from dataclasses import dataclass
 
 # Database and flask app:
 app = Flask(__name__)
@@ -15,7 +18,8 @@ app.config['SECRET_KEY'] = 'hugoguh12321'
 app.app_context().push()
 
 bcrypt = Bcrypt(app)
-db = SQLAlchemy(app)
+db = SQLAlchemy()
+db.init_app(app)
 
 # Database code
 
@@ -25,7 +29,7 @@ login_manager.login_view = 'login'
 
 @login_manager.user_loader
 def load_user(user_id):
-	return User.query.get(int(user_id))
+	return db.session.get(User, int(user_id))
 
 # User table:
 class User(db.Model, UserMixin):
@@ -36,49 +40,84 @@ class User(db.Model, UserMixin):
 	password = db.Column(db.String(100), nullable=False)
 
 # Posts table:
+@dataclass
 class Post(db.Model, UserMixin):
-	id = db.Column(db.Integer, primary_key=True)
-	idUser = db.Column(db.Integer)
+	# Post Attributes
+	id: int = db.Column(db.Integer, primary_key=True)
+	userId: int = db.Column(db.Integer, nullable=False)
+	username: str = db.Column(db.String(100), nullable=False)
+	location: str = db.Column(db.String(100), nullable=False)
 
 	# Pet Attributes
-	name = db.Column(db.String(100), nullable=False)
-	race = db.Column(db.String(100), nullable=False)
-	age = db.Column(db.Integer, nullable=False)
-	sex = db.Column(db.String(2), nullable=False)
-	size = db.Column(db.String(100), nullable=False)
+	petName: str = db.Column(db.String(100), nullable=False)
+	petRace: str = db.Column(db.String(100), nullable=False)
+	petAge: int = db.Column(db.Integer, nullable=False)
+	petSex: str = db.Column(db.String(2), nullable=False)
+	petSize: str = db.Column(db.String(100), nullable=False)
 
 # Register form
 # Table insertion:
 class RegisterForm(FlaskForm):
-	username = StringField(validators=[InputRequired(), Length(min=8, max=100)], render_kw={"placeholder": "Nome completo"})
-	email = EmailField('Endereço de Email', [validators.DataRequired(), validators.Email()], render_kw={"placeholder": "Email"})
-	password = PasswordField(validators=[InputRequired(), Length(min=8, max=100)], render_kw={"placeholder": "Senha"})
+	username = StringField(validators=[InputRequired(), Length(min=8, max=100)])
+	email = EmailField('Endereço de Email', [validators.DataRequired(), validators.Email()])
+	password = PasswordField(validators=[InputRequired(), Length(min=8, max=100)])
 	
 	submit = SubmitField("Registrar")
 
 	def validate_email(self, email):
-		existing_user = User.query.filter_by(email=email.data).first()
+		existing_user = db.session.execute(db.select(User).filter_by(email=email.data)).scalar_one_or_none()
 		if existing_user:
 			raise ValidationError("Esse usuário já existe!")
 
 # Login form
 # Table insertion:
 class LoginForm(FlaskForm):
-	email = EmailField('Endereço de Email', [validators.DataRequired(), validators.Email()], render_kw={"placeholder": "Email"})
-	password = PasswordField(validators=[InputRequired(), Length(min=8, max=100)], render_kw={"placeholder": "Senha"})
+	email = EmailField('Endereço de Email', [validators.DataRequired(), validators.Email()])
+	password = PasswordField(validators=[InputRequired(), Length(min=8, max=100)])
 
 	submit = SubmitField("Login")
 
 	def validate_email(self, email):
-		existing_user = User.query.filter_by(email=email.data).first()
-		if not existing_user:
+		existing_user = db.session.execute(db.select(User).filter_by(email=email.data)).scalar_one_or_none()
+		if existing_user is None:
 			raise ValidationError("Esse usuário não existe!")
 		
 	def validate_password(self, password):
-		existing_user = User.query.filter_by(email=self.email.data).first()
+		existing_user = db.session.execute(db.select(User).filter_by(email=self.email.data)).scalar_one_or_none()
 		if existing_user and not bcrypt.check_password_hash(existing_user.password, password.data):
 			raise ValidationError("Senha incorreta!")
 		self.user = existing_user
+
+# Post form
+# Table insertion:
+class PostForm(FlaskForm):
+	location = SelectField('Município', choices=[], validate_choice=False)
+
+	petName = StringField('Nome do Pet', validators=[InputRequired(), Length(min=3, max=100)])
+	petRace = SelectField('Raça do Pet', choices=[
+		'Border Collie', 
+		'Pitbull',
+		'Chihuahua',
+		'Labrador',
+		'Vira-Lata'
+	])
+	petAge = SelectField('Idade', choices=[
+		'Recém-nascido a 6 meses',
+		'6 meses a 1 ano',
+		'1 ano a 6 anos',
+		'6 anos ou mais'
+	])
+	petSex = SelectField('Sexo', choices=[
+		'Macho',
+		'Fêmea'
+	])
+	petSize = SelectField('Porte', choices=[
+		"Grande",
+		"Médio",
+		"Pequeno"
+	])
+
+	submit = SubmitField("Postar")
 
 # Route all pages
 @app.route('/')
@@ -91,6 +130,7 @@ def login():
 
 	if form.validate_on_submit():
 		session['user'] = {
+			'id': form.user.id,
 			'email': form.user.email,
 			'username': form.user.username
 		}
@@ -121,7 +161,29 @@ def logout():
 @app.route('/dashboard', methods=['GET', 'POST'])
 @login_required
 def dashboard():
-	return render_template('dashboard.html', session=session)
+	postForm = PostForm()
+
+	if postForm.validate_on_submit():
+		new_post = Post(
+			username = session['user']['username'],
+			userId = session['user']['id'],
+			location = postForm.location.data,
+
+			petName = postForm.petName.data,
+			petRace = postForm.petRace.data,
+			petAge = postForm.petAge.data,
+			petSex = postForm.petSex.data,
+			petSize = postForm.petSize.data,
+		)
+		db.session.add(new_post)
+		db.session.commit()
+		return redirect(url_for('dashboard'))
+	return render_template('dashboard.html', postForm=postForm, session=session)
+
+@app.route('/posts')
+def posts():
+	posts = db.session.execute(db.select(Post)).scalars().all()
+	return jsonify(posts)
 
 @app.route('/manifest.json')
 def serve_manifest():
--- static/scripts/dashboard.js
+++ static/scripts/dashboard.js
@@ -1,4 +1,78 @@
-document.addEventListener("DOMContentLoaded", function () {
+function create_post(post) {
+	let post_template = document.querySelector("#post_template")
+	let clone = post_template.content.cloneNode(true)
 
+	let posterName = clone.querySelector('#posterName');
+	let posterLocation = clone.querySelector('#posterLocation');
+
+	let petName = clone.querySelector('#petName');
+	let petRace = clone.querySelector('#petRace');
+	let petAge = clone.querySelector('#petAge');
+	let petSex = clone.querySelector('#petSex');
+	let petSize = clone.querySelector('#petSize');
+
+	posterLocation.innerText = ""
+	posterName.innerText = post.username
+
+	petName.innerText = "Nome: " + post.petName
+	petRace.innerText = "Raça: " + post.petRace
+	petAge.innerText = "Idade: " + post.petAge
+	petSex.innerText = "Sexo: " + post.petSex
+	petSize.innerText = "Porte: " + post.petSize
+
+	return clone
+}
+
+async function fetch_posts() {
+	try {
+		let feeds = document.getElementsByClassName("feed")
+
+		const response = await fetch("/posts")
+		const data = await response.json()
+
+		feeds[0].innerHTML = ""
+		feeds[1].innerHTML = ""
+
+		data.forEach(post => {
+			let clone_1 = create_post(post)
+			let clone_2 = create_post(post)
+
+			feeds[0].appendChild(clone_1)
+			feeds[1].appendChild(clone_2)
+		})
+	} catch (error) {
+		console.error("Error fetching posts:", error)
+	}
+	return
+}
+
+async function load_towns() {
+	const uf_select = document.querySelector("#uf_select")
+	let town_select = document.querySelector("#townSelector")
+	town_select.innerHTML = ""
+
+	try {
+		const towns = await fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${uf_select.value}/municipios`)
+		const data = await towns.json()
+
+		data.forEach(town => {
+			let townNode = document.createElement("option")
+			townNode.value = town.nome
+			townNode.innerHTML = town.nome
+			town_select.appendChild(townNode)
+		})
+	} catch (error) {
+		console.error("Error fetching towns:", error)
+	}
+	return
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+	setInterval(fetch_posts, 6000)
+	fetch_posts()
+	load_towns()
+
+	const uf_select = document.querySelector("#uf_select")
+	uf_select.addEventListener("change", load_towns)
 	return
 })
