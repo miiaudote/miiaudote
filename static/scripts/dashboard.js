bb6143d8aa67d147989f317793e00c09d242e5ed
Emanuel Bevilaqua <beviemanuel@gmail.com>
2025-05-11 09:43:42 -0300

    Fixed bugs

--- static/scripts/dashboard.js
+++ static/scripts/dashboard.js
@@ -1,4 +1,4 @@
-function create_post(post) {
+function create_post(post, mobile) {
 	let post_template = document.querySelector("#post_template")
 	let clone = post_template.content.cloneNode(true)
 
@@ -13,19 +13,41 @@ function create_post(post) {
 	let petSize = clone.querySelector("#petSize")
 
 	let filenames = JSON.parse(post.images)
+	let first_image = true
+
+	let postParentElement = postImages.parentElement
+	let prev_button = postParentElement.querySelector(".carousel-control-prev")
+	let next_button = postParentElement.querySelector(".carousel-control-next")
+	let postTargetId = "carousel_post_"+post.id
+
+	if (mobile) {
+		postTargetId += "mobile"
+	}
+
+	postParentElement.id = postTargetId
+	prev_button.setAttribute("data-bs-target", "#"+postTargetId)
+	next_button.setAttribute("data-bs-target", "#"+postTargetId)
+
 	filenames.forEach(filename => {
 		let carousel_item = document.createElement("div")
-		carousel_item.classList = ["carousel-item active"]
-
+		carousel_item.style = "height: 50vh;"
+		if (first_image) {
+			carousel_item.classList = ["carousel-item active"]
+		}
+		else {
+			carousel_item.classList = ["carousel-item"]
+		}
+		
 		let carousel_item_img = document.createElement("img")
-		carousel_item_img.classList = ["d-block w-100"]
+		carousel_item_img.classList = ["d-block h-100 w-100 object-fit-fill"]
 		carousel_item_img.src = `uploads/${filename}`
-
+		
 		carousel_item.appendChild(carousel_item_img)
 		postImages.appendChild(carousel_item)
+		first_image = false
 	})
 
-	posterLocation.innerText = ""
+	posterLocation.innerText = post.location
 	posterName.innerText = post.username
 
 	petName.innerText = "Nome: " + post.petName
@@ -48,7 +70,7 @@ async function fetch_posts() {
 
 		data.forEach(post => {
 			let clone_1 = create_post(post)
-			let clone_2 = create_post(post)
+			let clone_2 = create_post(post, true)
 
 			feeds[0].appendChild(clone_1)
 			feeds[1].appendChild(clone_2)
@@ -81,7 +103,7 @@ async function load_towns() {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-	setInterval(fetch_posts, 30400)
+	setInterval(fetch_posts, 3000)
 	fetch_posts()
 	load_towns()
 
